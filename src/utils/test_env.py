58171248766b1e4749df92658d13f48ec58513e3
PinkySmile <Gegel85@laposte.net>
2025-12-27 12:31:52 +0100

    Added auto play in test env

--- src/utils/test_env.py
+++ src/utils/test_env.py
@@ -4,6 +4,20 @@ from PokeBattle.Gen1.Env import Examples
 import gymnasium as gym
 import asyncio
 import sys
+from PokeBattle.Gen1.State import PlayerState, BattleAction
+
+
+def get_move(me: PlayerState) -> BattleAction:
+	my_moves = me.pokemon_on_field.move_set
+	if me.pokemon_on_field.health == 0:
+		return BattleAction(BattleAction.Switch1 + next(i for i in range(len(me.team)) if me.team[i].health != 0 and me.pokemon_on_field_index != i))
+	if me.pokemon_on_field.wrapped:
+		if sum(p.health != 0 for p in me.team) == 1:
+			return BattleAction.NoAction
+		return BattleAction(BattleAction.Switch1 + next(i for i in range(len(me.team)) if me.team[i].health != 0 and me.pokemon_on_field_index != i))
+	if all(m.pp == 0 and i != me.pokemon_on_field.move_disabled for i, m in enumerate(my_moves)):
+		return BattleAction.StruggleMove
+	return BattleAction.Attack1 + next(i for i, m in enumerate(my_moves) if m.pp != 0)
 
 
 async def ainput(string: str) -> str:
@@ -20,9 +34,10 @@ def check_obs(p, obs):
 			continue
 		print(f"#{i} not in range: {low[i]} <= {obs[i]} <= {high[i]}")
 
+auto = len(sys.argv) > 1
 
 async def main():
-	p = gym.make('PokemonYellow', render_mode="human", shuffle_teams=True, rom="/home/pinky/pokeyellow-gen-II/pokeyellow.gbc", leak_state=True)
+	p = gym.make('PokemonYellow', render_mode="rgb_array_list", shuffle_teams=True, rom="/home/pinky/pokeyellow-gen-II/pokeyellow.gbc", leak_state=True, skip_frames=300, use_emulator=False)
 	finished = False
 	params = Examples.Blue3_1
 	observation, info = p.reset(options=params)
@@ -30,24 +45,31 @@ async def main():
 	print(observation, info, len(observation))
 	check_obs(p, observation)
 	r = p.render()
-	if r:
+	if isinstance(r, str):
 		print(r)
 	while not finished:
 		didit = False
 		while not didit:
-			future = asyncio.ensure_future(ainput("Action index:"))
-			while not future.done():
-				if info['emulator'] is not None:
-					info['emulator'].tick()
-				await asyncio.sleep(0)
-			info['simulator'].save_replay("interrupted.replay")
+			if not auto:
+				future = asyncio.ensure_future(ainput("Action index:"))
+				while not future.done():
+					if info['emulator'] is not None:
+						info['emulator'].tick()
+					await asyncio.sleep(0)
+			else:
+				action = get_move(info['simulator'].state.me)
+				if action >= BattleAction.Attack1 + 10:
+					action = action - BattleAction.NoAction + 10
+				else:
+					action = action - BattleAction.Attack1
 			while not didit:
 				try:
-					action = future.result()
-					if action == "stop":
-						info['simulator'].save_replay("interrupted.replay")
-						exit(1)
-					action = int(future.result())
+					if not auto:
+						action = future.result()
+						if action == "stop":
+							info['simulator'].save_replay("interrupted.replay")
+							exit(1)
+						action = int(future.result())
 					if not info['mask'][action]:
 						print("Warning: using disabled action")
 					observation, reward, finished, truncated, info = p.step(action)
@@ -65,9 +87,9 @@ async def main():
 			info['simulator'].save_replay("interrupted.replay")
 
 		r = p.render()
-		if r:
+		if isinstance(r, str):
 			print(r)
-		print(observation, info, len(observation))
+		print(observation, info, len(observation), reward, finished, truncated)
 		check_obs(p, observation)
 
 asyncio.run(main())
